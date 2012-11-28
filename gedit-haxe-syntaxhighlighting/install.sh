0ab62f92e2c526eeca8ca24fecf7abd72f2ac5aa
adnez7@gmail.com <adnez7@gmail.com@03755383-b929-9798-1e98-e337ed9f0eb6>
2012-11-28 03:50:30 +0000

    (04/03/2012) Modified install files

--- gedit-haxe-syntaxhighlighting/install.sh
+++ gedit-haxe-syntaxhighlighting/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+LANGSPEC_FILE="haxe.lang"
+MIME_FILE="haxe.xml"
 
 ROOT_LANGSPEC_FOLDER="/usr/share/gtksourceview-3.0/language-specs"
 ROOT_MIME_FOLDER="/usr/share/mime"
@@ -8,53 +10,38 @@ USER_LANGSPEC_FOLDER="$HOME/.local/share/gtksourceview-3.0/language-specs"
 USER_MIME_FOLDER="$HOME/.local/share/mime"
 USER_MIME_PACKAGES_FOLDER="$HOME/.local/share/mime/packages"
 
-LANGSPEC_FILE="haxe.lang"
-MIME_FILE="haxe.xml"
+if [ `whoami` = 'root' ]; then
+	LANGSPEC_FOLDER=$ROOT_LANGSPEC_FOLDER
+	MIME_FOLDER=$ROOT_MIME_FOLDER
+	MIME_PACKAGES_FOLDER=$ROOT_MIME_PACKAGES_FOLDER
+else
+	LANGSPEC_FOLDER=$USER_LANGSPEC_FOLDER
+	MIME_FOLDER=$USER_MIME_FOLDER
+	MIME_PACKAGES_FOLDER=$USER_MIME_PACKAGES_FOLDER
+fi
 
 echo "*** Installing haXe syntax file for highlighting ***"
 
-#create directories for the plugin files
-if [ `whoami` != 'root' ]; then
+if [ ! -d $LANGSPEC_FOLDER ]; then
+	mkdir -p "$LANGSPEC_FOLDER"
+fi
 
-	if [ ! -d $USER_LANGSPEC_FOLDER ]; then
-		mkdir -p "$USER_LANGSPEC_FOLDER"
-	fi
-	
-	if [ ! -d $USER_MIME_PACKAGES_FOLDER ]; then
-		mkdir -p "$USER_MIME_PACKAGES_FOLDER"
-	fi
-	
+if [ ! -d $MIME_PACKAGES_FOLDER ]; then
+		mkdir -p "$MIME_PACKAGES_FOLDER"
 fi
 
 
-if [ `whoami` = 'root' ]; then
-
-	if [ ! -f "$ROOT_MIME_PACKAGES_FOLDER/$MIME_FILE" ]; then
-		echo "Adding $MIME_FILE, to: $ROOT_MIME_PACKAGES_FOLDER"
-		cp "$MIME_FILE" "$ROOT_MIME_PACKAGES_FOLDER/$MIME_FILE"
-	fi
+if [ ! -f "$MIME_PACKAGES_FOLDER/$MIME_FILE" ]; then
+	echo "Adding file: $MIME_FILE, to: $MIME_PACKAGES_FOLDER"
+	cp "$MIME_FILE" "$MIME_PACKAGES_FOLDER/$MIME_FILE"
+fi
 	
-	if [ ! -f "$ROOT_LANGSPEC_FOLDER/$LANGSPEC_FILE" ]; then
-		echo "Adding $LANGSPEC_FILE, to: $ROOT_LANGSPEC_FOLDER"
-		cp "$LANGSPEC_FILE" "$ROOT_LANGSPEC_FOLDER/$LANGSPEC_FILE"
-	fi
-	echo "updating mime-database"
-	update-mime-database "$ROOT_MIME_FOLDER"
-	
-else
-
-	if [ ! -f "$USER_MIME_PACKAGES_FOLDER/$MIME_FILE" ]; then
-		echo "Adding $MIME_FILE, to: $USER_MIME_PACKAGES_FOLDER"
-		cp "$MIME_FILE" "$USER_MIME_PACKAGES_FOLDER/$MIME_FILE"
-	fi
-	
-	if [ ! -f "$USER_LANGSPEC_FOLDER/$LANGSPEC_FILE" ]; then
-		echo "Adding $LANGSPEC_FILE, to: $USER_LANGSPEC_FOLDER"
-		cp "$LANGSPEC_FILE" "$USER_LANGSPEC_FOLDER/$LANGSPEC_FILE"
-	fi
-	echo "updating mime-database"
-	update-mime-database "$USER_MIME_FOLDER"
-
+if [ ! -f "$LANGSPEC_FOLDER/$LANGSPEC_FILE" ]; then
+	echo "Adding file: $LANGSPEC_FILE, to: $LANGSPEC_FOLDER"
+	cp "$LANGSPEC_FILE" "$LANGSPEC_FOLDER/$LANGSPEC_FILE"
 fi
 
-echo "Done installing.\n"
+echo "updating mime-database"
+update-mime-database "$MIME_FOLDER"
+
+echo "Done installing haxe syntax file.\n"
