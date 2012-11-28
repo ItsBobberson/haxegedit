75f5c5466ba7e678eb619faa86f419248f9e80a8
adnez7@gmail.com <adnez7@gmail.com@03755383-b929-9798-1e98-e337ed9f0eb6>
2012-11-28 03:22:19 +0000

    (10/02/2012) Split the syntax highlighting from the code completion plugin.

---
+++ gedit-haxe-syntaxhighlighting/install.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+ROOT_LANGSPEC_FOLDER="/usr/share/gtksourceview-3.0/language-specs"
+ROOT_MIME_FOLDER="/usr/share/mime"
+ROOT_MIME_PACKAGES_FOLDER="/usr/share/mime/packages"
+
+USER_LANGSPEC_FOLDER="$HOME/.local/share/gtksourceview-3.0/language-specs"
+USER_MIME_FOLDER="$HOME/.local/share/mime"
+USER_MIME_PACKAGES_FOLDER="$HOME/.local/share/mime/packages"
+
+LANGSPEC_FILE="haxe.lang"
+MIME_FILE="haxe.xml"
+
+#create directories for the plugin files
+if [ `whoami` != 'root' ]; then
+
+	if [ ! -d $USER_LANGSPEC_FOLDER ]; then
+		mkdir -p "$USER_LANGSPEC_FOLDER"
+	fi
+	
+	if [ ! -d $USER_MIME_PACKAGES_FOLDER ]; then
+		mkdir -p "$USER_MIME_PACKAGES_FOLDER"
+	fi
+	
+fi
+
+
+if [ `whoami` = 'root' ]; then
+
+	if [ ! -f "$ROOT_MIME_PACKAGES_FOLDER/$MIME_FILE" ]; then
+		echo "Adding $MIME_FILE to $ROOT_MIME_PACKAGES_FOLDER"
+		cp "$MIME_FILE" "$ROOT_MIME_PACKAGES_FOLDER/$MIME_FILE"
+	fi
+	
+	if [ ! -f "$ROOT_LANGSPEC_FOLDER/$LANGSPEC_FILE" ]; then
+		echo "Adding $LANGSPEC_FILE to $ROOT_LANGSPEC_FOLDER"
+		cp "$LANGSPEC_FILE" "$ROOT_LANGSPEC_FOLDER/$LANGSPEC_FILE"
+	fi
+	echo "updating mime-database"
+	update-mime-database "$ROOT_MIME_FOLDER"
+	
+else
+
+	if [ ! -f "$USER_MIME_PACKAGES_FOLDER/$MIME_FILE" ]; then
+		echo "Adding $MIME_FILE to $USER_MIME_PACKAGES_FOLDER"
+		cp "$MIME_FILE" "$USER_MIME_PACKAGES_FOLDER/$MIME_FILE"
+	fi
+	
+	if [ ! -f "$USER_LANGSPEC_FOLDER/$LANGSPEC_FILE" ]; then
+		echo "Adding $LANGSPEC_FILE to $USER_LANGSPEC_FOLDER"
+		cp "$LANGSPEC_FILE" "$USER_LANGSPEC_FOLDER/$LANGSPEC_FILE"
+	fi
+	echo "updating mime-database"
+	update-mime-database "$USER_MIME_FOLDER"
+
+fi
+
+echo "*** Done installing the haXe syntax file for highlighting ***"
